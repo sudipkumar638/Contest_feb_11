64201eaedaaecaeb88c141f4a3b816bbf1eb0ead
sudipt <sudiptkumar638@gmail.com>
2023-02-11 10:28:59 +0530

    Javascript code added Successfully

--- script.js
+++ script.js
@@ -7,21 +7,43 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+
+  arr.map((item, ind) => {
+    if (item.marks > 50) {
+      console.log(item)
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((item, ind) => {
+    if (item.marks > 50) {
+      console.log(item)
+    }
+  })
 }
 
 function addData() {
-  //Write your code here, just console.log
+
+  const data = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(data);
+  console.log(arr)
+
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((item, ind) => {
+    return item.marks >= 50;
+  })
+
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const students = [{ id: 5, name: "sudipt", age: "25", marks: 51 }, { id: 6, name: "kunal", age: "22", marks: 49 }, { id: 7, name: "amit", age: "20", marks: 50 }]
+  arr.push.apply(arr, students)
+
+  console.log(arr)
 }
